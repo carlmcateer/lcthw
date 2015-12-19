569d53e0b82a2a643110c97c752ef8375351e4b0
CarlMcAteer <carlmc12@gmail.com>
2015-12-19 23:11:18 +0000

    first commit

---
+++ ex1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	puts("Hello World");
+	puts("Hi Planet");
+	puts("Holla Bros");
+	puts("Sup Land's");
+	puts("Another Line");
+	return 0;
+}
